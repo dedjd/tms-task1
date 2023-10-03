ce81e8bafd28d74791254da9e97740c040bd5479
dedjd <146872003+dedjd@users.noreply.github.com>
2023-10-03 23:15:41 +0300

    Created project

---
+++ ConsoleTmsTask1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Привет, как тебя зовут?");
+var name = Console.ReadLine();
+
+if (name == "Олег")
+{
+    Console.WriteLine("До свидания, Олег!");
+}
+else
+{
+    Console.WriteLine("Очень приятно, " + name + "!");
+    Console.WriteLine("Наш список цен:");
+
+    for (var i = 1; i <= 3; i++)
+    {
+        Console.WriteLine("Кружка " + i + "50мл. - " + int.Parse(i + "50") / 50 + " руб.");
+    }
+
+    Console.WriteLine("Сколько кружек лимонада ты хочешь?");
+    var count = int.Parse(Console.ReadLine());
+    Console.WriteLine("Какого объёма?");
+    var volume = int.Parse(Console.ReadLine());
+
+    if (volume == 150 || volume == 250 || volume == 350)
+    {
+        var price = volume / 50;
+        var totalPrice = count * price;
+        Console.WriteLine("С тебя " + totalPrice + " руб.");
+    }
+    else
+    {
+        Console.WriteLine("Кружки в " + volume + "мл. нет!");
+    }
+}
+
+Console.ReadLine();
