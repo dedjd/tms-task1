cdc352b7feefb597db6ee6871a37d76a66358c11
dedjd <146872003+dedjd@users.noreply.github.com>
2023-11-09 17:04:58 +0300

    Task 12

---
+++ TmsMvcTask12/Models/ProductListModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TmsMvc.Models;
+
+public class ProductListModel
+{
+    public ProductModel[] Products { get; set; }
+
+    public int Count { get; set; }
+
+    public int TotalCount { get; set; }
+
+    public decimal TotalCost { get; set; }
+}
---
+++ TmsMvcTask12/Services/IInventoryService.cs
@@ -0,0 +1,16 @@
+﻿using TmsMvc.Models;
+
+namespace TmsMvc.Services;
+
+public interface IInventoryService
+{
+    ProductListModel GetProducts();
+
+    CommandResultModel AddProduct(ProductModel product);
+
+    CommandResultModel DeleteProduct(SelectProductModel product);
+
+    CommandResultModel UpdateProduct(ProductModel product);
+
+    ProductModel GetProductById(Guid id);
+}
